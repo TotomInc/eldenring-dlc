098fcb7fad5ba72b8aa7a0f6886b3679208f02d1
Thomas Cazade <cazade.thomas@gmail.com>
2024-04-28 11:07:03 +0200

    fix: img path

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
       <div id="three-container" className="h-screen w-screen fixed">
-        <Image src="/elden-ring.webp" alt="" fill quality={90} sizes="100vw" className="object-cover" />
+        <Image src="/images/elden-ring.webp" alt="" fill quality={90} sizes="100vw" className="object-cover" />
         <span className="fixed bg-black inset-0 bg-opacity-50" />
         <ParticleBackground />
       </div>
