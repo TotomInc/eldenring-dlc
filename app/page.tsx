19f83e3f1c73d796e36e56c07112630bddda1498
Thomas Cazade <cazade.thomas@gmail.com>
2024-04-28 10:45:40 +0200

    feat: meta-og, minor improvements

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
       <div id="three-container" className="h-screen w-screen fixed">
-        <Image src="/elden-ring.webp" alt="" fill unoptimized className="object-cover opacity-60" />
+        <Image src="/elden-ring.webp" alt="" fill quality={90} sizes="100vw" className="object-cover opacity-60" />
         <ParticleBackground />
       </div>
 
